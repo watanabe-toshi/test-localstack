b0edba7db30d7137281071b8f9e1f9e0837080f6
watanabe-toshi <109321773+watanabe-toshi@users.noreply.github.com>
2026-03-11 21:41:20 +0900

    Add SQS queue and DynamoDB table resources

--- main.tf
+++ main.tf
@@ -9,14 +9,40 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    s3 = "http://localhost:4566"
+    s3       = "http://localhost:4566"
+    sqs      = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
   }
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "localstack-terraform-bucket"
+  bucket        = "localstack-terraform-bucket"
+  force_destroy = true
+}
+
+resource "aws_sqs_queue" "example" {
+  name = "localstack-terraform-queue"
+}
+
+resource "aws_dynamodb_table" "example" {
+  name         = "localstack-terraform-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
 }
 
 output "bucket_name" {
   value = aws_s3_bucket.example.bucket
+}
+
+output "queue_url" {
+  value = aws_sqs_queue.example.url
+}
+
+output "table_name" {
+  value = aws_dynamodb_table.example.name
 }
