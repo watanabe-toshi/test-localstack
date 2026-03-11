81afe8c34234ca292549777431d0d3b83e67202a
watanabe-toshi <109321773+watanabe-toshi@users.noreply.github.com>
2026-03-11 22:02:45 +0900

    Enable S3 path style and update endpoints

--- main.tf
+++ main.tf
@@ -3,11 +3,13 @@ provider "aws" {
   access_key                  = "test"
   secret_key                  = "test"
 
+  s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
   endpoints {
+    s3  = "http://localhost:4566"
     sns = "http://localhost:4566"
     sqs = "http://localhost:4566"
   }
