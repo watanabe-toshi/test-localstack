f9b4fce43da9844f3fe14c7845e3b5bfe15263b9
watanabe-toshi <109321773+watanabe-toshi@users.noreply.github.com>
2026-03-11 21:52:30 +0900

    Add SNS topic and SQS queue policy
    Added SNS topic and associated resources for SQS integration.

--- main.tf
+++ main.tf
@@ -9,40 +9,61 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    s3       = "http://localhost:4566"
-    sqs      = "http://localhost:4566"
-    dynamodb = "http://localhost:4566"
+    sns = "http://localhost:4566"
+    sqs = "http://localhost:4566"
   }
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket        = "localstack-terraform-bucket"
-  force_destroy = true
 }
 
 resource "aws_sqs_queue" "example" {
   name = "localstack-terraform-queue"
 }
 
-resource "aws_dynamodb_table" "example" {
-  name         = "localstack-terraform-table"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
+resource "aws_sns_topic" "example" {
+  name = "localstack-terraform-topic"
+}
 
-  attribute {
-    name = "id"
-    type = "S"
+data "aws_iam_policy_document" "allow_sns_to_sqs" {
+  statement {
+    sid    = "Allow-SNS-SendMessage"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["sns.amazonaws.com"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = [aws_sqs_queue.example.arn]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_sns_topic.example.arn]
+    }
   }
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.example.bucket
+resource "aws_sqs_queue_policy" "example" {
+  queue_url = aws_sqs_queue.example.id
+  policy    = data.aws_iam_policy_document.allow_sns_to_sqs.json
+}
+
+resource "aws_sns_topic_subscription" "example" {
+  topic_arn = aws_sns_topic.example.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.example.arn
+
+  depends_on = [aws_sqs_queue_policy.example]
+}
+
+output "topic_arn" {
+  value = aws_sns_topic.example.arn
 }
 
 output "queue_url" {
-  value = aws_sqs_queue.example.url
+  value = aws_sqs_queue.example.id
 }
 
-output "table_name" {
-  value = aws_dynamodb_table.example.name
+output "queue_arn" {
+  value = aws_sqs_queue.example.arn
 }
